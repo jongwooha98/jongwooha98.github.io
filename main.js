c87a11d71055abab1f48783213c041e638b0b5f7
JongWoo Ha <jongwooha98@gmail.com>
2017-08-02 19:39:40 +0900

    modified main-section

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-var keywords = ["adaptable", "practical", "considerate", "rational", "ambitious", "impartial", "unassuming", "generous", "questioning why you are still staring at this."];
+/*-----home-section: change of keyword & color-----*/
+var keywords = ["adaptable", "practical", "considerate", "rational", "ambitious", "impartial", "unassuming", "generous", "questioning why you're still staring at this."];
 var count = 1;
 setInterval(function(){    
-    $("span.keyword").fadeOut(400, function(){        
+    $("span.keyword").fadeOut(300, function(){        
         $(this).html(keywords[count]);        
         count++;        
         if(count == keywords.length)            
-            count = 0;        
-        $(this).fadeIn(400);    
+            count = 0;
+    var hue = 'rgb('
+        + (Math.floor(Math.random() * 256)) + ','
+        + (Math.floor(Math.random() * 256)) + ','
+        + (Math.floor(Math.random() * 256)) + ')';      
+        $(this).fadeIn(300).css('color', hue);    
     });
 }, 2000);
